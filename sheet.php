27044d6f7dd35c9aed504c82db9d0c798f6f4b35
Prakhar0123 <136454803+Prakhar0123@users.noreply.github.com>
2024-07-20 00:36:36 +0530

    Update sheet.php

--- sheet.php
+++ sheet.php
@@ -14,7 +14,7 @@ $counter=0;
 
 while($emp=mysqli_fetch_assoc($fetchingEmp)){
     $empArr[]= $emp['emp_name'];
-    $idArr[]= $emp['id'];
+    $idArr[]= $emp['count'];
 }
 
 
@@ -28,23 +28,9 @@ while($emp=mysqli_fetch_assoc($fetchingEmp)){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Report</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">    </head>
 </head>
 <body>
     
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-
-<div class="container-fluid">
-    <header class="bg-danger text-white text-center mb-3 py-3">
-        <div class="row">
-            <div class="col-12">
-                <h1>KRYPTON ATTENDANCE</h1>
-                <h3>EMPLOYEES ATTENDANCE FOR THE MONTH: <u><?php echo strtoupper(date("F")); ?></u></h3>
-                </div>
-        </div>
-    </header>
 <table border="1" cellspacing="0">
     <?php
     for($i=1;$i<=$totalNumberOfEmp+2;$i++){
